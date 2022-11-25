595d12fbb34a21a60cb09821bafa73a83e333f07
cvb272 <112788751+cvb272@users.noreply.github.com>
2022-11-25 22:12:10 +0900

    Update button.py

--- button.py
+++ button.py
@@ -77,3 +77,28 @@ b5 = Button(frame3, text = "마지막", command = go_last).grid(row = 1, column 
 b6 = Button(frame3, text = "파일 읽기", command = load).grid(row = 1, column = 6)
 
 window.mainloop() 
+
+#5
+inFileName = input("입력 파일 이름: ")
+outFileName = input("출력 파일 이름: ")
+
+infile = open(inFileName, "r")
+outfile = open(outFileName, "w")
+
+total = 0.0
+count = 0
+
+line = infile.readline()
+while line != "" :
+   value = float(line)
+   total = total + value
+   count = count + 1
+   line = infile.readline()
+     
+outfile.write("합계="+ str(total)+"\n")
+
+avg = total / count
+outfile.write("평균="+ str(avg)+"\n")
+
+infile.close()
+outfile.close()
